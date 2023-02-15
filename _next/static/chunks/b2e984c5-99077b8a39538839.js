31f30b16b5dd3ac5b6ab0c9b0ff5d1c424053bd1
evshiron <evshiron@gmail.com>
2023-02-15 23:45:02 +0800

    deploy

---
+++ _next/static/chunks/b2e984c5-99077b8a39538839.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[702],{5317:function(n,u,e){e.d(u,{J3k:function(){return L}});var L="M20,11V13H8L13.5,18.5L12.08,19.92L4.16,12L12.08,4.08L13.5,5.5L8,11H20Z"}}]);
